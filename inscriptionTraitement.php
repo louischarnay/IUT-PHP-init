44e4674f4ac748e7baefe3e897a72772d9ab9e16
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2021-11-29 14:53:58 +0100

    ajout messages mail/mdp incorrect inscription

--- inscriptionTraitement.php
+++ inscriptionTraitement.php
@@ -2,13 +2,19 @@
 session_start();
 include "class/db.php";
 $db1 = new db();
-if ($db1->createAccount($_POST["email"], $_POST["prenom"], $_POST["nom"], $_POST["mdp"], $_POST["mdp2"], $_POST["ville"], $_POST["code_postal"], $_POST["adresse"]) == true) {
+$result = $db1->createAccount($_POST["email"], $_POST["prenom"], $_POST["nom"], $_POST["mdp"], $_POST["mdp2"], $_POST["ville"], $_POST["code_postal"], $_POST["adresse"]);
+if ($result == 0) {
     $_SESSION['connected'] = true;
     $_SESSION['nom'] = $db1->getNom($_POST["email"]);
     $_SESSION['prenom'] = $db1->getPrenom($_POST["email"]);
+    $_SESSION['inscriptionFailed'] = "fine";
     header("Location: ../index.php");
 }
-else {
-    var_dump("Inscription failed");
+else if($result == 1){
+    $_SESSION['inscriptionFailed'] = "wrongPassword";
+    header("Location: ../inscription.php");
 }
-?>
+else if($result == 2){
+    $_SESSION['inscriptionFailed'] = "wrongMail";
+    header("Location: ../inscription.php");
+}
