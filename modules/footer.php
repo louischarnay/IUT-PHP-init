0297002b675febf6b40bc17f75f6c380ce040504
Louis Charnay <louis.charnay@orange.fr>
2022-01-28 15:08:31 +0100

    modified copyright

--- modules/footer.php
+++ modules/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <img src="../img/logoUCBL.png" id="logoUCBL" alt="Logo Université Claude Bernard Lyon 1">
     <div id="footerP">
-        <p>Copyright © Pablo Guinard 2021</p>
+        <p>Copyright © Pablo Guinard - Louis Charnay 2021</p>
         <p>Tous droits d'auteur réservés</p>
     </div>
     <div id="footerA">
